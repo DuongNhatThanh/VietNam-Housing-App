71a0521e6266d3e48f1230d35e829b4a39b4b378
DuongNhatThanh <142218914+DuongNhatThanh@users.noreply.github.com>
2023-09-19 07:32:51 +0700

    Add files via upload

---
+++ App.py
@@ -0,0 +1,31 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from screens.index import get_routes
+
+st.set_page_config(
+    layout="wide",
+)
+
+routes = get_routes()
+
+with st.sidebar:
+    global selected_screen
+    
+    selected_screen = option_menu("Taskbar", routes['name'], 
+        icons=routes['icon'], menu_icon="book")
+    
+    # selected_screen = on_hover_tabs(tabName=routes['name'], 
+    #                      iconName=routes['icon'], default_choice=0)
+
+routes['component'][selected_screen]()
+
+
+
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
